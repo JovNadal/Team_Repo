348e2ddb813fd1e6d865a8938f0c97a791b943f0
JovanElias <nadaljovanelias@gmail.com>
2025-03-19 15:04:00 +0800

    Create settings.py
    added settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+# Sample structure settings.py
+# Make sure these fields exist in your .env
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DJANGO_SECRET_KEY:str
+    DJANGO_DEBUG:int
+    DJANGO_ALLOWED_HOSTS:str
+    CSRF_ALLOWED_HOSTS:str
+    DB_NAME:str
+    DB_HOST:str
+    DB_USER:str
+    DB_PASSWORD:str
+    DB_PORT:int
+
+    model_config = {
+        "env_file": ".env",
+        "case_sensitive": True,
+        "extra": "allow"
+    }
+        
+settings = Settings()
