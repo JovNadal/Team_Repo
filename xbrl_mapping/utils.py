d7614eba1afa1dbca0583d97ac4c4419c5c610ec
Dreyyy25 <almosaraaj25@gmail.com>
2025-03-20 18:17:24 +0800

    Update utils.py
    Added data parameter on success_response.

--- xbrl_mapping/utils.py
+++ xbrl_mapping/utils.py
@@ -1,21 +1,41 @@
 from rest_framework.response import Response
 from rest_framework import status
 
-def success_response(message: str, status_code=status.HTTP_200_OK):
+def success_response(message: str, data=None, status_code=status.HTTP_200_OK):
     """
     Returns a standardized success response
+    
+    Args:
+        message (str): Main success message
+        data (dict, optional): Additional data to include in response
+        status_code (int, optional): HTTP status code
+    
+    Returns:
+        Response: Django REST Framework Response object
     """
     response_body = {
         "success": True,
         "message": message
     }
     
+    if data is not None:
+        response_body["data"] = data
+    
     return Response(response_body, status=status_code)
 
 
-def error_response(message: str, error=None, status_code=status.HTTP_400_BAD_REQUEST):
+def error_response(message: str, error=None, data=None, status_code=status.HTTP_400_BAD_REQUEST):
     """
     Returns a standardized error response
+    
+    Args:
+        message (str): Main error message
+        error (str, optional): Error details
+        data (dict, optional): Additional data to include in response
+        status_code (int, optional): HTTP status code
+    
+    Returns:
+        Response: Django REST Framework Response object
     """
     response_body = {
         "success": False,
@@ -24,5 +44,8 @@ def error_response(message: str, error=None, status_code=status.HTTP_400_BAD_REQ
     
     if error is not None:
         response_body["error"] = error
+        
+    if data is not None:
+        response_body["data"] = data
     
     return Response(response_body, status=status_code)
